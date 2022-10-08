627001330d1741ab8a9d949b51288bed84cd0668
Andrew Ellen <18004410@uhi.ac.uk>
2022-10-08 04:06:05 +0100

    Able to detect some typos and shortened words but still misses most

--- search_function.py
+++ search_function.py
@@ -1,58 +1,62 @@
 
-def wordSnippets(searchWordLength):
-    snippetCountx3 = 0
-    snippetCountx2 = 0
+def wordSnippets(wordLength):
     snippetCountx1 = 0
+    snippetCountx2 = 0
+    snippetCountx3 = 0
 
-    snippetCountx3 = searchWordLength // 3
-    searchWordLength -= snippetCountx3*3
+    snippetCountx3 = wordLength // 3
+    wordLength -= snippetCountx3*3
 
-    snippetCountx2 = searchWordLength // 2
-    searchWordLength -= snippetCountx2*2
+    snippetCountx2 = wordLength // 2
+    wordLength -= snippetCountx2*2
 
-    snippetCountx1 = searchWordLength // 1
+    snippetCountx1 = wordLength // 1
 
-    print(snippetCountx3, snippetCountx2, snippetCountx1)
-    return snippetCountx3, snippetCountx2, snippetCountx1
+    return [snippetCountx3, snippetCountx2, snippetCountx1]
 
-def main(list, word):
-    searchWordLength = len(word)
-    snippetCountx3, snippetCountx2, snippetCountx1 = wordSnippets(searchWordLength)
-    for listWord in list:
-        listWordLength = len(listWord)
-        listWordSimilarityScore = 0
-
-        for i in range(searchWordLength):
-            try:
+def wordSimilarityFunc(snippets, searchWord, comparisonWord):
+    wordSimilarity = 0
+    for i in range(3):
+        for x in range(snippets[i]):
+            snippetSimilarity = 0
+            for letterNumber in range(len(searchWord[0:3-i])):
                 try:
-                    listOfLetters = word[i:i+3]
-                except:
-                    try:
-                        listOfLetters = word[i:i+2]
-                    except:
-                        listOfLetters = word[i]
-
-                listOfLettersLength = len(listOfLetters)
-                if listOfLettersLength == 0:
-                    continue
-                listOfLettersSimilarityScore = 0
-
-                for letter in listOfLetters:
-                    wordComparisonSnippet = listWord[i:i+listOfLettersLength]
-                    if letter in wordComparisonSnippet:
-                        listOfLettersSimilarityScore += 1
-                        continue
+                    if searchWord[letterNumber] == comparisonWord[letterNumber]:
+                        snippetSimilarity += 1
                     else:
-                        continue
+                        snippetSimilarity -= 1
+                except:
+                    snippetSimilarity -= 1
+                    continue
+            if snippetSimilarity >= (3-i)/2:
+                wordSimilarity += 1
+            else:
+                wordSimilarity -= 1
+            searchWord = searchWord[3-i:len(searchWord)]
+            comparisonWord = comparisonWord[3-i:len(comparisonWord)]
+    return wordSimilarity
 
-                if listOfLettersSimilarityScore >= listOfLettersLength/2:
-                    listWordSimilarityScore += 1
-                else:
-                    listWordSimilarityScore -= 1
-            except:
-                pass
-        if listWordSimilarityScore >= listWordLength/2:
-            print(listWord)
+def main(listOfWords, searchWord):
+    foundWordList = []
+
+    #Possibly Redundant
+    if searchWord in listOfWords:
+        foundWordList.append(listOfWords[listOfWords.index(searchWord)])
+        return foundWordList
+
+
+    searchWordLength = len(searchWord)
+    numberOfSearchWordSnippets = wordSnippets(searchWordLength)
+
+    for word in listOfWords:
+        wordSimilarity = wordSimilarityFunc(numberOfSearchWordSnippets, searchWord, word)
+        if wordSimilarity >= sum(numberOfSearchWordSnippets)/3:
+            foundWordList.append(word)
+
+    return foundWordList
+
+
+
 
 
 if __name__ == '__main__':
@@ -65,4 +69,10 @@ if __name__ == '__main__':
         "typical"
     ]
     word = input("Enter search term - ")
-    main(listOfWords, word.lower())
+    foundWordList = main(listOfWords, word.lower())
+    try:
+        for word in foundWordList:
+            print(word)
+    except:
+        print("No Instances")
+
