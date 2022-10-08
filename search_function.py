fa04f876bed374944364aa18f5f78ebcbe557275
Andrew Ellen <18004410@uhi.ac.uk>
2022-10-08 05:08:39 +0100

    Added a word similarity percentage but its inaccurate so far

--- search_function.py
+++ search_function.py
@@ -15,7 +15,7 @@ def wordSnippets(wordLength):
 
     return [snippetCountx3, snippetCountx2, snippetCountx1]
 
-def wordSimilarityFunc(snippets, searchWord, comparisonWord):
+def wordSimilarityFunc(snippets, searchWord, comparisonWord, searchSensitivity):
     wordSimilarity = 0
     for i in range(3):
         for x in range(snippets[i]):
@@ -26,7 +26,7 @@ def wordSimilarityFunc(snippets, searchWord, comparisonWord):
                         snippetSimilarity += 1
                 except:
                     continue
-            if snippetSimilarity >= (3-i)/2:
+            if snippetSimilarity >= (3-i)/searchSensitivity:
                 wordSimilarity += 1
             else:
                 wordSimilarity -= 1
@@ -34,12 +34,12 @@ def wordSimilarityFunc(snippets, searchWord, comparisonWord):
             comparisonWord = comparisonWord[3-i:len(comparisonWord)]
     return wordSimilarity
 
-def main(listOfWords, searchWord):
+def main(listOfWords, searchWord, searchSensitivity):
     foundWordList = []
 
     #Fast search for word
     if searchWord in listOfWords:
-        foundWordList.append(searchWord)
+        foundWordList.append([searchWord, 100])
         del(listOfWords[listOfWords.index(searchWord)])
 
     searchWordLength = len(searchWord)
@@ -47,9 +47,13 @@ def main(listOfWords, searchWord):
 
     #Searching for any words similar
     for word in listOfWords:
-        wordSimilarity = wordSimilarityFunc(numberOfSearchWordSnippets, searchWord, word)
-        if wordSimilarity >= sum(numberOfSearchWordSnippets)/3:
-            foundWordList.append(word)
+        wordSimilarity = wordSimilarityFunc(numberOfSearchWordSnippets, searchWord, word, searchSensitivity)
+
+        percentage = 100/sum(numberOfSearchWordSnippets)
+        wordSimilarityPercentage = percentage*wordSimilarity
+
+        if wordSimilarityPercentage >= 45:
+            foundWordList.append([word,wordSimilarityPercentage])
 
     return foundWordList
 
@@ -59,8 +63,13 @@ def main(listOfWords, searchWord):
 
 if __name__ == '__main__':
     listOfWords = [i.lower() for i in english_words_set]
+
+    print(max(listOfWords, key=len))
+
     word = input("Enter search term - ")
-    foundWordList = main(listOfWords, word.lower())
+    #searchSensitivity controls how close each snippet has to be to the searched word. 3 is low, 1 is high
+    searchSensitivity = 1
+    foundWordList = main(listOfWords, word.lower(), searchSensitivity)
     try:
         for word in foundWordList:
             print(word)
